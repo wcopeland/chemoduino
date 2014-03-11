2b69c367d7175be98911987ee40c6945af9efce3
Wilbert Copeland <wilbertcopeland@gmail.com>
2014-03-11 10:28:16 -0700

    v1.0
    - Added syntax checking
    - Added "-help" command
    - Added "-exec" command

--- Python/PyArdComm_27.py
+++ Python/PyArdComm_27.py
@@ -3,6 +3,7 @@ __author__ = 'Wilbert'
 
 import struct
 import os
+import re
 import serial
 from serial.tools import list_ports
 
@@ -37,6 +38,93 @@ def packAsFloat(value):
 
 
 
+def CheckSyntax(text):
+    if len(text) > 63:
+        # All commands must be less than 64 bytes.
+        return False
+
+    if len([x for x in text if x == '-']) > 5:
+        # Commands may contain a maximum of 4 arguements.
+        return False
+
+    argument_lists = {'-get':['-wpin', '-rpin', '-power', '-pfreq', '-pwid'],
+                      '-set':['-wpin', '-rpin', '-power', '-pfreq', '-pwid'],
+                      '-start':['-cont', '-auto', '-pulse'],
+                      'stop':[''],
+                      '-exec':[''],
+                      '-help':[''],}
+    command_list = [k for k in argument_lists]
+
+    command_list = '(' + ')|('.join(command_list) + ')'
+    if not re.match(command_list, text):
+        return False
+
+    command = re.match(command_list, text).group(0)
+    if command == '-get':
+        m = re.match('-get [0-5] (.*)', text)
+        if m is None:
+            return False
+        elif m.group(1) not in argument_lists['-get']:
+            return False
+
+    elif command == '-set':
+        m = re.match('-set [0-5] (.*) \d+\.*\d*(.*)', text)
+        if m is None:
+            return False
+        elif m.group(1) not in argument_lists['-set']:
+            return False
+        elif m.group(2) != '':
+            return False
+
+    elif command == '-start':
+        m = re.match('-start [0-5] (.*)', text)
+        if m is None:
+            return False
+        elif m.group(1) not in argument_lists['-start']:
+            return False
+
+    elif command == '-stop':
+        m = re.match('-stop [0-5](.*)', text)
+        if m is None:
+            return False
+        elif m.group(1) not in argument_lists['-stop']:
+            return False
+
+    elif command == '-exec':
+        m = re.match('-exec (\w+.txt)(.*)', text)
+        if m is None:
+            return False
+        elif m.group(2) not in argument_lists['-exec']:
+            return False
+        try:
+            file = open(m.group(1), 'r')
+        except:
+            return False
+        # Check each line for validity
+        command_queue = []
+        for line in file:
+            if not CheckSyntax(line):
+                return False
+            command_queue.append(line.replace('\n',''))
+            return command_queue
+
+    elif command == '-help':
+        print('To start pumps...')
+        print('-start <pump_id> <arg>')
+        print('Arguments: -cont, -auto, -pulse\n')
+
+        print('To stop pumps...')
+        print('-stop <pump_id>\n')
+
+        print('To set pump attribute...')
+        print('-set <pump_id> <arg> <val>')
+        print('Arguements: -wpin, -rpin, -pfreq, -pwid, -power\n')
+
+        print('To get pump attribute...')
+        print('-set <pump_id> <arg>')
+        print('Arguements: -wpin, -rpin, -pfreq, -pwid, -power\n\n')
+
+    return True
 
 print('Available ports: {0}'.format(','.join([x for x in ListSerialPorts()])))
 current_port = raw_input('Which port would you like to connect to? ')
@@ -45,27 +133,27 @@ while(current_port not in ListSerialPorts()):
     current_port = raw_input('Port "{0}" not found.\nWhich port would you like to connect to? '.format(current_port))
 
 
-
 ser = serial.Serial(current_port, 9600)
-#ser = serial.Serial('COM11', 9600)
 ser.setDTR()
 ser.setRTS()
 
 command = raw_input('Enter command: ')
 while command != 'exit':
 
-    if len(command) > 63:
-        print('Commands must be less than 64 bytes!')
+    valid = CheckSyntax(command)
+    if valid is False:
+        print('Invalid command.')
         command = raw_input('Enter command: ')
         continue
-
-    if len([x for x in command if x == '-']) > 5:
-        print('Commands must not pass more than 4 arguements!')
+    elif type(valid) is list:
+        for command in valid:
+            ser.write(command)
+            print(ser.readline())
         command = raw_input('Enter command: ')
-        continue
-
-    ser.write(command)
-    print(ser.readline())
-    command = raw_input('Enter command: ')
+    else:
+        # Send command
+        ser.write(command)
+        print(ser.readline())
+        command = raw_input('Enter command: ')
 
 ser.close()
